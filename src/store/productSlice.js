503e6958479a88e976402aa197443295727946a8
Aswin <aswinkrishna363@gmail.com>
2023-11-28 10:47:19 +0530

    added the asyncThunk middleware and updated their status

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,7 +1,8 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   data: [],
+  status: 'idle'
 }
 
 const productSlice = createSlice({
@@ -11,16 +12,38 @@ const productSlice = createSlice({
     fetchProducts(state, action) {
       state.data = action.payload
     }
+  },
+
+  extraReducers: (builder) => {
+
+    builder.addCase(getProducts.pending, (state, action) => {
+      state.status = 'loading'
+    })
+
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.data = action.payload
+      state.status = 'fulfilled'
+    })
+
+    builder.addCase(getProducts.rejected, (state, action) => {
+      state.status = 'rejected'
+    })
   }
 })
 
 export const { fetchProducts } = productSlice.actions
 export default productSlice.reducer
 
-export function getProducts() {
-  return async function getProductsThunk(dispatch, getState) {
-    const data = await fetch("https://fakestoreapi.com/products");
-    const response = await data.json();
-    dispatch(fetchProducts(response))
-  }
-}
+export const getProducts = createAsyncThunk('product/get', async () => {
+  const data = await fetch("https://fakestoreapi.com/products");
+  const response = await data.json();
+  return response;
+})
+
+// export function getProducts() {
+//   return async function getProductsThunk(dispatch, getState) {
+//     const data = await fetch("https://fakestoreapi.com/products");
+//     const response = await data.json();
+//     dispatch(fetchProducts(response))
+//   }
+// }
